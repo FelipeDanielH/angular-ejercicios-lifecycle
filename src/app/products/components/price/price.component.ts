6b0e14df196e276f815078e4385c74a0f919bce5
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-29 21:24:34 -0300

    feat(precio): agrega ejemplo de intervalo pasando por su creacion hasta su destruccion

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -10,14 +11,21 @@ export class PriceComponent implements OnInit,OnChanges,OnDestroy{
   @Input()
   public price:number = 0;
 
+  public interval$?: Subscription;
+
   ngOnInit(): void {
     console.log("HIJO: ngOnInit");
+
+    this.interval$ = interval(1000).subscribe( value => console.log(`Tick: ${ value }`))
   }
+
   ngOnChanges(changes: SimpleChanges): void {
     console.log("HIJO: ngOnChanges");
     console.log({changes});
   }
+
   ngOnDestroy(): void {
     console.log("HIJO: ngOnDestroy");
+    this.interval$?.unsubscribe()
   }
 }
