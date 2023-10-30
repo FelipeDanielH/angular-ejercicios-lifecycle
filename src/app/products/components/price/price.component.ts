d18738ab69abdbf4d3437d5b4761f44d2de589c0
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-29 21:04:32 -0300

    feat(productos): implementa metodos del ciclo de vida

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css']
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit,OnChanges,OnDestroy{
 
+  @Input()
+  public price:number = 0;
+
+  ngOnInit(): void {
+    console.log("HIJO: ngOnInit");
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("HIJO: ngOnChanges");
+    console.log({changes});
+  }
+  ngOnDestroy(): void {
+    console.log("HIJO: ngOnDestroy");
+  }
 }
