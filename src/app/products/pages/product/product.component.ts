3b5feb138aaa1245bd4627fec7e03924c0b320b8
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-29 21:05:10 -0300

    feat(productos): implementa visualizacion de metodos del ciclo de vida e interaccion con precio

--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
+
+  constructor() {
+    console.log('PADRE: constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('PADRE: ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({changes});
+    console.log('PADRE:ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('PADRE:ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('PADRE:ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('PADRE:ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('PADRE:ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('PADRE:ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('PADRE:ngOnDestroy');
+  }
+
+  increasePrice() {
+    this.currentPrice++
+  }
 
 }
